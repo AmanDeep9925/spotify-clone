62767d8b8b834becdf0d52644af5a272fe04067a
AmanDeep9925 <amandeep58257@gmail.com>
2020-09-02 19:41:33 +0530

    Added the Sidebar to Player

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebaroption from './Sidebaroption';
+import './sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>sidebar</h1>
+            <Sidebaroption />
+        </div>
+    );
+}
+
+export default Sidebar;
