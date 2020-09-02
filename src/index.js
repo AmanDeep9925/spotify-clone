6d330b62ba4fe81290842e812dfd3e5da0c65a0b
AmanDeep9925 <amandeep58257@gmail.com>
2020-09-02 19:01:18 +0530

    Added datalayer to App

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import DataLayer from './DataLayer';
+
+import reducer, { initialState } from './reducers/reducers';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <DataLayer initalState={initialState} reducer = {reducer}>
+            <App />
+        </DataLayer>
     </React.StrictMode>,
     document.getElementById('root')
 );
