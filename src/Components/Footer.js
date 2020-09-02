2154021ae6a72d08dff8868cd31a539ac932861e
AmanDeep9925 <amandeep58257@gmail.com>
2020-09-02 19:42:39 +0530

    Added the Footer Component

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className = "footer">
+            <h1>I'm Footer</h1>
+        </div>
+    )
+}
+
+export default Footer
